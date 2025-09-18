f36d49fcfcd767d696592277fb77cdf82aa24f34
DexLovecraft <101021602+DexLovecraft@users.noreply.github.com>
2025-09-18 18:18:29 +0200

    fix jwt

--- auth.js
+++ auth.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
     try {
         const token = req.headers.authorization.split(' ')[1];
-        const decodedToken = jwt.verify(token, '$2a$12$dCCL6qWZNv1Zr5UpY4NGNOLlAcTcAalOQyU0YB9K35J5fwHhwnt');
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
         const userId = decodedToken.userId;
         req.auth = {
             userId: userId
